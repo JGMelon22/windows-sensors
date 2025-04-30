e3ae83aa5e5e0c626eed81e4708ed3c5756789ae
JGMelon22 <jgvasconcellos22@gmail.com>
2025-04-30 07:30:34 -0300

    chore: remove unused comments and apply previous hatrdware list method

--- Sensors/Services/HardwareMonitorService.cs
+++ Sensors/Services/HardwareMonitorService.cs
@@ -19,51 +19,29 @@ public class HardwareMonitorService
         computer.Open();
         computer.Accept(new UpdateVisitor());
 
-        //HardwareType[] supportedHardwareTypes =
-        //[
-        //    HardwareType.Cpu,
-        //    HardwareType.GpuNvidia,
-        //    HardwareType.GpuAmd,
-        //    HardwareType.GpuIntel
-        //];
-
-
-
-        //foreach (IHardware hardware in computer.Hardware)
-        //{
-        //    // Include CPU and all GPU types (discrete and integrated)
-        //    if (supportedHardwareTypes.Contains(hardware.HardwareType))
-        //    {
-        //        Console.WriteLine("Hardware: {0}", hardware.Name);
-
-        //        foreach (ISensor sensor in hardware.Sensors)
-        //        {
-        //            // Filter for temperature sensors
-        //            if (sensor.SensorType == SensorType.Temperature)
-        //            {
-        //                Console.WriteLine("\tSensor: {0}, Temperature: {1:F2}°C", sensor.Name, sensor.Value);
-        //            }
-        //        }
-        //    }
-        //}
+        HardwareType[] supportedHardwareTypes =
+        [
+            HardwareType.Cpu,
+            HardwareType.GpuNvidia,
+            HardwareType.GpuAmd,
+            HardwareType.GpuIntel
+        ];
 
         foreach (IHardware hardware in computer.Hardware)
         {
-            Console.WriteLine("Hardware: {0}", hardware.Name);
-
-            foreach (IHardware subhardware in hardware.SubHardware)
+            // Include CPU and all GPU types (discrete and integrated)
+            if (supportedHardwareTypes.Contains(hardware.HardwareType))
             {
-                Console.WriteLine("\tSubhardware: {0}", subhardware.Name);
+                Console.WriteLine("Hardware: {0}", hardware.Name);
 
-                foreach (ISensor sensor in subhardware.Sensors)
+                foreach (ISensor sensor in hardware.Sensors)
                 {
-                    Console.WriteLine("\t\tSensor: {0}, value: {1:F2}°C", sensor.Name, sensor.Value);
+                    // Filter for temperature sensors
+                    if (sensor.SensorType == SensorType.Temperature)
+                    {
+                        Console.WriteLine("\tSensor: {0}, Temperature: {1:F2}°C", sensor.Name, sensor.Value);
+                    }
                 }
-            }
-
-            foreach (ISensor sensor in hardware.Sensors)
-            {
-                Console.WriteLine("\tSensor: {0}, value: {1:F2}°C", sensor.Name, sensor.Value);
             }
         }
 
